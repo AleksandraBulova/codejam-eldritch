0e7d35bdb5188b39c2677edaa7351d8d044ba129
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2022-08-24 12:34:04 +0300

    feat: add game cards

---
+++ src/components/Cards/dataCards.js
@@ -0,0 +1,361 @@
+import blue1 from '../../assets/MythicCards/blue/blue1.png'
+import blue2 from '../../assets/MythicCards/blue/blue2.png'
+import blue3 from '../../assets/MythicCards/blue/blue3.png'
+import blue4 from '../../assets/MythicCards/blue/blue4.png'
+import blue5 from '../../assets/MythicCards/blue/blue5.png'
+import blue6 from '../../assets/MythicCards/blue/blue6.png'
+import blue7 from '../../assets/MythicCards/blue/blue7.png'
+import blue8 from '../../assets/MythicCards/blue/blue8.png'
+import blue9 from '../../assets/MythicCards/blue/blue9.png'
+import blue10 from '../../assets/MythicCards/blue/blue10.png'
+import blue11 from '../../assets/MythicCards/blue/blue11.png'
+import blue12 from '../../assets/MythicCards/blue/blue12.png'
+import brown1 from '../../assets/MythicCards/brown/brown1.png'
+import brown2 from '../../assets/MythicCards/brown/brown2.png'
+import brown3 from '../../assets/MythicCards/brown/brown3.png'
+import brown4 from '../../assets/MythicCards/brown/brown4.png'
+import brown5 from '../../assets/MythicCards/brown/brown5.png'
+import brown6 from '../../assets/MythicCards/brown/brown6.png'
+import brown7 from '../../assets/MythicCards/brown/brown7.png'
+import brown8 from '../../assets/MythicCards/brown/brown8.png'
+import brown9 from '../../assets/MythicCards/brown/brown9.png'
+import brown10 from '../../assets/MythicCards/brown/brown10.png'
+import brown11 from '../../assets/MythicCards/brown/brown11.png'
+import brown12 from '../../assets/MythicCards/brown/brown12.png'
+import brown13 from '../../assets/MythicCards/brown/brown13.png'
+import brown14 from '../../assets/MythicCards/brown/brown14.png'
+import brown15 from '../../assets/MythicCards/brown/brown15.png'
+import brown16 from '../../assets/MythicCards/brown/brown16.png'
+import brown17 from '../../assets/MythicCards/brown/brown17.png'
+import brown18 from '../../assets/MythicCards/brown/brown18.png'
+import brown19 from '../../assets/MythicCards/brown/brown19.png'
+import brown20 from '../../assets/MythicCards/brown/brown20.png'
+import brown21 from '../../assets/MythicCards/brown/brown21.png'
+import green1 from '../../assets/MythicCards/green/green1.png'
+import green2 from '../../assets/MythicCards/green/green2.png'
+import green3 from '../../assets/MythicCards/green/green3.png'
+import green4 from '../../assets/MythicCards/green/green4.png'
+import green5 from '../../assets/MythicCards/green/green5.png'
+import green6 from '../../assets/MythicCards/green/green6.png'
+import green7 from '../../assets/MythicCards/green/green7.png'
+import green8 from '../../assets/MythicCards/green/green8.png'
+import green9 from '../../assets/MythicCards/green/green9.png'
+import green10 from '../../assets/MythicCards/green/green10.png'
+import green11 from '../../assets/MythicCards/green/green11.png'
+import green12 from '../../assets/MythicCards/green/green12.png'
+import green13 from '../../assets/MythicCards/green/green13.png'
+import green14 from '../../assets/MythicCards/green/green14.png'
+import green15 from '../../assets/MythicCards/green/green15.png'
+import green16 from '../../assets/MythicCards/green/green16.png'
+import green17 from '../../assets/MythicCards/green/green17.png'
+import green18 from '../../assets/MythicCards/green/green18.png'
+
+
+export const cardsData = [
+  {
+    id: 'blue1',
+    difficulty: 'hard',
+    color:'blue',
+    image: blue1
+  },
+  {
+    id: 'blue2',
+    difficulty: 'hard',
+    color:'blue',
+    image: blue2
+  },
+  {
+    id: 'blue3',
+    difficulty: 'easy',
+    color:'blue',
+    image: blue3
+  },
+  {
+    id: 'blue4',
+    difficulty: 'easy',
+    color:'blue',
+    image: blue4
+  },
+  {
+    id: 'blue5',
+    difficulty: 'easy',
+    color:'blue',
+    image: blue5
+  },
+  {
+    id: 'blue6',
+    difficulty: 'hard',
+    color:'blue',
+    image: blue6
+  },
+  {
+    id: 'blue7',
+    difficulty: 'normal',
+    color:'blue',
+    image: blue7
+  },
+  {
+    id: 'blue8',
+    difficulty: 'hard',
+    color:'blue',
+    image: blue8
+  },
+  {
+    id: 'blue9',
+    difficulty: 'normal',
+    color:'blue',
+    image: blue9
+  },
+  {
+    id: 'blue10',
+    difficulty: 'easy',
+    color:'blue',
+    image: blue10
+  },
+  {
+    id: 'blue11',
+    difficulty: 'normal',
+    color:'blue',
+    image: blue11
+  },
+  {
+    id: 'blue12',
+    difficulty: 'normal',
+    color:'blue',
+    image: blue12
+  },
+  {
+    id: 'brown1',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown1
+  },
+  {
+    id: 'brown2',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown2
+  },
+  {
+    id: 'brown3',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown3
+  },
+  {
+    id: 'brown4',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown4
+  },
+  {
+    id: 'brown5',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown5
+  },
+  {
+    id: 'brown6',
+    difficulty: 'hard',
+    color:'brown',
+    image: brown6
+  },
+  {
+    id: 'brown7',
+    difficulty: 'hard',
+    color:'brown',
+    image: brown7
+  },
+  {
+    id: 'brown8',
+    difficulty: 'hard',
+    color:'brown',
+    image: brown8
+  },
+  {
+    id: 'brown9',
+    difficulty: 'hard',
+    color:'brown',
+    image: brown9
+  },
+  {
+    id: 'brown10',
+    difficulty: 'hard',
+    color:'brown',
+    image: brown10
+  },
+  {
+    id: 'brown11',
+    difficulty: 'easy',
+    color:'brown',
+    image: brown11
+  },
+  {
+    id: 'brown12',
+    difficulty: 'easy',
+    color:'brown',
+    image: brown12
+  },
+  {
+    id: 'brown13',
+    difficulty: 'easy',
+    color:'brown',
+    image: brown13
+  },
+  {
+    id: 'brown14',
+    difficulty: 'easy',
+    color:'brown',
+    image: brown14
+  },
+  {
+    id: 'brown15',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown15
+  },
+  {
+    id: 'brown16',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown16
+  },
+  {
+    id: 'brown17',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown17
+  },
+  {
+    id: 'brown18',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown18
+  },
+  {
+    id: 'brown19',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown19
+  },
+  {
+    id: 'brown20',
+    difficulty: 'normal',
+    color:'brown',
+    image: brown20
+  },
+  {
+    id: 'brown21',
+    difficulty: 'easy',
+    color:'brown',
+    image: brown21
+  },
+  {
+    id: 'green1',
+    difficulty: 'easy',
+    color:'green',
+    image: green1
+  },
+  {
+    id: 'green2',
+    difficulty: 'hard',
+    color:'green',
+    image: green2
+  },
+  {
+    id: 'green3',
+    difficulty: 'hard',
+    color:'green',
+    image: green3
+  },
+  {
+    id: 'green4',
+    difficulty: 'hard',
+    color:'green',
+    image: green4
+  },
+  {
+    id: 'green5',
+    difficulty: 'hard',
+    color:'green',
+    image: green5
+  },
+  {
+    id: 'green6',
+    difficulty: 'hard',
+    color:'green',
+    image: green6
+  },
+  {
+    id: 'green7',
+    difficulty: 'normal',
+    color:'green',
+    image: green7
+  },
+  {
+    id: 'green8',
+    difficulty: 'normal',
+    color:'green',
+    image: green8
+  },
+  {
+    id: 'green9',
+    difficulty: 'normal',
+    color:'green',
+    image: green9
+  },
+  {
+    id: 'green10',
+    difficulty: 'normal',
+    color:'green',
+    image: green10
+  },
+  {
+    id: 'green11',
+    difficulty: 'normal',
+    color:'green',
+    image: green11
+  },
+  {
+    id: 'green12',
+    difficulty: 'easy',
+    color:'green',
+    image: green12
+  },
+  {
+    id: 'green13',
+    difficulty: 'normal',
+    color:'green',
+    image: green13
+  },
+  {
+    id: 'green14',
+    difficulty: 'normal',
+    color:'green',
+    image: green14
+  },
+  {
+    id: 'green15',
+    difficulty: 'normal',
+    color:'green',
+    image: green15
+  },
+  {
+    id: 'green16',
+    difficulty: 'easy',
+    color:'green',
+    image: green16
+  },
+  {
+    id: 'green17',
+    difficulty: 'easy',
+    color:'green',
+    image: green17
+  },
+  {
+    id: 'green18',
+    difficulty: 'easy',
+    color:'green',
+    image: green18
+  }
+]
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,4 +1,5 @@
-import { curentState } from "../../utils"
+import { ancientsData } from "../../utils"
+import { cardsData } from "../Cards/dataCards"
 
 export const BUTTON_CREATE_CARD_DESK = document.querySelector('.button-create_card-deck')
 export const WRAPPER_GAME = document.querySelector('.wrapper_game')
@@ -8,4 +9,75 @@ BUTTON_CREATE_CARD_DESK.addEventListener('click', () => {
   WRAPPER_GAME.classList.add('active')
   curentState()
 })
+
+const arrayCardsDeskBlue = []
+const arrayCardsDeskBrown = []
+const arrayCardsDeskGreen = []
+
+for(let i = 0; i < cardsData.length; i++) {
+  if(cardsData[i].color === 'blue') {
+    arrayCardsDeskBlue.push( cardsData[i].image)
+  }
+  if(cardsData[i].color === 'brown') {
+    arrayCardsDeskBrown.push( cardsData[i].image)
+  }
+  if(cardsData[i].color === 'green') {
+    arrayCardsDeskGreen.push( cardsData[i].image)
+  }
+}
+
+const arrayGame = []
+
+export const curentState = () => {
+  if(document.querySelector('.Azathoth').classList.contains('active')) {
+    document.querySelector('.dot_green1').innerHTML = ancientsData[0].firstStage.greenCards
+    document.querySelector('.dot_brown1').innerHTML = ancientsData[0].firstStage.brownCards
+    document.querySelector('.dot_blue1').innerHTML = ancientsData[0].firstStage.blueCards
+    document.querySelector('.dot_green2').innerHTML = ancientsData[0].secondStage.greenCards
+    document.querySelector('.dot_brown2').innerHTML = ancientsData[0].secondStage.brownCards
+    document.querySelector('.dot_blue2').innerHTML = ancientsData[0].secondStage.blueCards
+    document.querySelector('.dot_green3').innerHTML = ancientsData[0].thirdStage.greenCards
+    document.querySelector('.dot_brown3').innerHTML = ancientsData[0].thirdStage.brownCards
+    document.querySelector('.dot_blue3').innerHTML = ancientsData[0].thirdStage.blueCards
+  }
+  if(document.querySelector('.Cthulthu').classList.contains('active')) {
+    document.querySelector('.dot_green1').innerHTML = ancientsData[1].firstStage.greenCards
+    document.querySelector('.dot_brown1').innerHTML = ancientsData[1].firstStage.brownCards
+    document.querySelector('.dot_blue1').innerHTML = ancientsData[1].firstStage.blueCards
+    document.querySelector('.dot_green2').innerHTML = ancientsData[1].secondStage.greenCards
+    document.querySelector('.dot_brown2').innerHTML = ancientsData[1].secondStage.brownCards
+    document.querySelector('.dot_blue2').innerHTML = ancientsData[1].secondStage.blueCards
+    document.querySelector('.dot_green3').innerHTML = ancientsData[1].thirdStage.greenCards
+    document.querySelector('.dot_brown3').innerHTML = ancientsData[1].thirdStage.brownCards
+    document.querySelector('.dot_blue3').innerHTML = ancientsData[1].thirdStage.blueCards
+  }
+  if(document.querySelector('.LogSothoth').classList.contains('active')) {
+    document.querySelector('.dot_green1').innerHTML = ancientsData[2].firstStage.greenCards
+    document.querySelector('.dot_brown1').innerHTML = ancientsData[2].firstStage.brownCards
+    document.querySelector('.dot_blue1').innerHTML = ancientsData[2].firstStage.blueCards
+    document.querySelector('.dot_green2').innerHTML = ancientsData[2].secondStage.greenCards
+    document.querySelector('.dot_brown2').innerHTML = ancientsData[2].secondStage.brownCards
+    document.querySelector('.dot_blue2').innerHTML = ancientsData[2].secondStage.blueCards
+    document.querySelector('.dot_green3').innerHTML = ancientsData[2].thirdStage.greenCards
+    document.querySelector('.dot_brown3').innerHTML = ancientsData[2].thirdStage.brownCards
+    document.querySelector('.dot_blue3').innerHTML = ancientsData[2].thirdStage.blueCards
+  }
+  if(document.querySelector('.ShubNiggurath').classList.contains('active')) {
+    document.querySelector('.dot_green1').innerHTML = ancientsData[3].firstStage.greenCards
+    document.querySelector('.dot_brown1').innerHTML = ancientsData[3].firstStage.brownCards
+    document.querySelector('.dot_blue1').innerHTML = ancientsData[3].firstStage.blueCards
+    document.querySelector('.dot_green2').innerHTML = ancientsData[3].secondStage.greenCards
+    document.querySelector('.dot_brown2').innerHTML = ancientsData[3].secondStage.brownCards
+    document.querySelector('.dot_blue2').innerHTML = ancientsData[3].secondStage.blueCards
+    document.querySelector('.dot_green3').innerHTML = ancientsData[3].thirdStage.greenCards
+    document.querySelector('.dot_brown3').innerHTML = ancientsData[3].thirdStage.brownCards
+    document.querySelector('.dot_blue3').innerHTML = ancientsData[3].thirdStage.blueCards
+  }
+}
+
+
+
+document.querySelector('.card').style.backgroundImage = `url(${arrayCardsDeskBlue[0]})`
+
+
 
